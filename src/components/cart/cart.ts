fb31e4c069df2c54c749387624cab7924f7784ae
Roman Lysiuk <gekalo1025@gmail.com>
2022-12-20 20:00:30 +0200

    feat:add clear Cart method

--- src/components/cart/cart.ts
+++ src/components/cart/cart.ts
@@ -120,5 +120,8 @@ class Cart {
   deletePromoCode(promo: string) {
     delete this.allUsedPromoCode[promo];
   }
+  clearCart(): void {
+    this.allProductCart = new Map();
+  }
 }
 export default Cart;
